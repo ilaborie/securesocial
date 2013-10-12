325e7fe2439e25d347a5e8fe88dfe2568a19db9a
Mike Slinn <mslinn@micronauticsresearch.com>
2013-10-12 05:18:33 -0700

    Update to Scala 2.10.3 and tweaked build

--- samples/scala/demo/project/Build.scala
+++ samples/scala/demo/project/Build.scala
@@ -3,14 +3,24 @@ import Keys._
 import play.Project._
 
 object ApplicationBuild extends Build {
-
     val appName         = "ssdemo-scala"
     val appVersion      = "1.0"
 
     val appDependencies = Seq(
-      "securesocial" %% "securesocial" % "master-mslinn"
+      "com.micronautics" %% "securesocial" % "2.1.1-SNAPSHOT" withSources
     )
     val main = play.Project(appName, appVersion, appDependencies).settings(
-      resolvers += Resolver.url("sbt-plugin-snapshots", new URL("http://repo.scala-sbt.org/scalasbt/sbt-plugin-snapshots/"))(Resolver.ivyStylePatterns)
+      scalaVersion := "2.10.3",
+
+      resolvers ++= Seq(
+        //Resolver.file("Local Repository", file(sys.env.get("PLAY21_HOME").map(_ + "/repository/local").getOrElse("")))(Resolver.ivyStylePatterns),
+        Resolver.url("play-plugin-releases", new URL("http://repo.scala-sbt.org/scalasbt/sbt-plugin-releases/"))(Resolver.ivyStylePatterns),
+        Resolver.url("play-plugin-snapshots", new URL("http://repo.scala-sbt.org/scalasbt/sbt-plugin-snapshots/"))(Resolver.ivyStylePatterns)
+      ),
+      scalacOptions ++= Seq("-deprecation", "-encoding", "UTF-8", "-feature", "-target:jvm-1.6", "-unchecked",
+      "-Ywarn-adapted-args", "-Ywarn-value-discard", "-Xlint"),
+      logBuffered in Test := false,
+      Keys.fork in Test := false,
+      parallelExecution in Test := false
     )
 }
